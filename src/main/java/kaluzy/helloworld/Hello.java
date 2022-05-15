39d1efb8fbf4c47c7f623032120d6c68ef31352b
kaluzy <kaluzyt@gmail.com>
2022-05-14 23:24:08 -0400

    Hello World in NetBean

---
+++ src/main/java/kaluzy/helloworld/Hello.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author kaluzy
+ * email: kaluzyt@gmail.com
+ * date: 5/14/22
+ * purpose: My first Hello world program in an IDE
+ */
+package kaluzy.helloworld;
+
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Kaluzy");
+    }
+}
